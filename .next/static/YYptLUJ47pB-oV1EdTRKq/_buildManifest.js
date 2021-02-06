33f50782b9bf665a39f965a4f8f44ac28227e12c
hyeleelee <hgoguma124@gmail.com>
2021-02-06 16:10:35 +0900

    module 삭제

--- .next/static/YYptLUJ47pB-oV1EdTRKq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F807cc180517e24b5f8d9.css","static\u002Fchunks\u002Fpages\u002Findex-61cd5dc394183fc2107d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ac06083fe6eb4a4413b1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
