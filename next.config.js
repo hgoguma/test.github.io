e36cd976ccbc40fa9d24e50f7d485c21a31221bf
hyeleelee <hgoguma124@gmail.com>
2021-02-06 15:55:07 +0900

    url 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     /* config options here */
-    assetPrefix: 'https://github.com/hgoguma/test_project.github.io.git'
+    assetPrefix: 'https://github.com/hgoguma/test.github.io'
 }
