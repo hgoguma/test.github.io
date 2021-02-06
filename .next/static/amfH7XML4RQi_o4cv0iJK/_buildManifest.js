3353d2aedee4b10f5e78f8e931ba2423ed9ea374
hyeleelee <hgoguma124@gmail.com>
2021-02-06 18:20:17 +0900

    deploy

---
+++ .next/static/amfH7XML4RQi_o4cv0iJK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F07f6cae4cd85d5d22307.css","static\u002Fchunks\u002Fpages\u002Findex-d5831b39efd77f50d7b8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ac06083fe6eb4a4413b1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
