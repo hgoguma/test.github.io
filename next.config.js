beb39c7ee42213488a88eb7de7c32dc3f771cfa5
hyeleelee <hgoguma124@gmail.com>
2021-02-14 12:18:35 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    webpack: config => {
-        config.resolve.modules.push(path.resolve('./'))
-
-        return config
-    },
+    // webpack: config => {
+    //     config.resolve.modules.push(path.resolve('./'))
+    //     return config
+    // },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
-    assetPrefix: 'https://github.com/hgoguma/test.github.io'
+    // assetPrefix: 'https://github.com/hgoguma/test.github.io'
 }
